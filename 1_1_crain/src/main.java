03e283fa286f78878d759f546484262d2790bc8d
rhkdrod12 <rhkdrod12@gmail.com>
2021-01-06 23:47:35 +0900

    -init

--- 1_1_crain/src/main.java
+++ 1_1_crain/src/main.java
@@ -2,6 +2,9 @@ import java.util.Stack;
 
 public class main {
 	public static void main(String[] args) {
+		
+		// 프로그래머스 1-1 : 크레인 인형뽑기
+		
 		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
 		int[] moves = {1,5,3,5,1,2,1,4};
 		
